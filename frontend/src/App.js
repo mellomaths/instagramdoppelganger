b9a02fbdd3fcb83ccfb2212dd0f8c383b4f1e54e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 20:56:45 -0300

    Add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
+
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes />
         </BrowserRouter>
     );
