e11e157102c94089c32fc4921209c7a54423a872
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 19:42:24 -0300

    Add request handler for saving new images on post request

--- server/src/controllers/PostController.js
+++ server/src/controllers/PostController.js
@@ -3,7 +3,18 @@ const Post = require('../models/Post');
 const index = async (req, res) => {};
 
 const create = async (req, res) => {
-    return res.json({ ok: true });
+    const { author, place, description, hashtags } = req.body;
+    const { filename: image } = req.file;
+
+    const post = await Post.create({
+        author,
+        place,
+        description,
+        hashtags,
+        image
+    });
+
+    return res.json({ post });
 };
 
 module.exports = {
