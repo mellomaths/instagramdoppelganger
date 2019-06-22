884cb4f204e20e12451aa2727bf1e207c1b7fa13
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 19:03:33 -0300

    Add dotenv and mongoose connecting to atlas mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3333;
+const mongoDBConnectionString =
+    process.env.NODE_ENV === 'production'
+        ? process.env.MONGODB_CONNECTION_STRING
+        : process.env.DEV_MONGODB_CONNECTION_STRING;
+
+mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
     return res.send({ message: 'Hello' });
