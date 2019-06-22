c3b87d6a94df416a04153a5fb0d6f971bf254376
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 20:01:30 -0300

    Add cors to server

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -12,6 +13,8 @@ const mongoDBConnectionString =
         : process.env.DEV_MONGODB_CONNECTION_STRING;
 
 mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
