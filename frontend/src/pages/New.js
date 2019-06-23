7de9cddb05078a7b0ea317882e6c97e84af0d7cb
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 22:21:19 -0300

    Add form to new post

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
 
+import './new.css';
+
 export default class New extends Component {
+    state = {
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hashtags: ''
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+
+    handleImageChange = e => {
+        this.setState({ image: e.target.files[0] });
+    };
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
     render() {
-        return <h1>New</h1>;
+        return (
+            <form id="new-post" onSubmit={this.handleSubmit}>
+                <input type="file" onChange={this.handleImageChange} />
+                <input
+                    type="text"
+                    name="author"
+                    placeholder="Author"
+                    onChange={this.handleChange}
+                    value={this.state.author}
+                />
+                <input type="text" name="place" placeholder="Place" onChange={this.handleChange} value={this.state.place} />
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Description"
+                    onChange={this.handleChange}
+                    value={this.state.description}
+                />
+                <input
+                    type="text"
+                    name="hashtags"
+                    placeholder="Hashtags"
+                    onChange={this.handleChange}
+                    value={this.state.hashtags}
+                />
+
+                <button type="submit">Send</button>
+            </form>
+        );
     }
 }
