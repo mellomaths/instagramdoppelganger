2b616eab0914828a7277e211ee94d7829bbd91ab
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 22:07:32 -0300

    Fetch data from the api with axios

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import api from '../services/api';
+
 import './feed.css';
 
 import more from '../assets/more.svg';
@@ -8,60 +10,48 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 export default class Feed extends Component {
+    state = {
+        feed: []
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({ feed: response.data.posts });
+    }
+
     render() {
+        const { feed } = this.state;
+
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Matheus Mello</span>
-                            <span className="place">Rio de Janeiro</span>
-                        </div>
+                {feed.map(post => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                        <img src={more} alt="More" />
-                    </header>
+                            <img src={more} alt="More" />
+                        </header>
 
-                    <img src="http://localhost:3333/files/204781.jpg" />
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} />
-                            <img src={comment} />
-                            <img src={send} />
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
 
-                        <strong>900 likes</strong>
-                        <p>
-                            Um post muito massa
-                            <span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Matheus Mello</span>
-                            <span className="place">Rio de Janeiro</span>
-                        </div>
-
-                        <img src={more} alt="More" />
-                    </header>
-
-                    <img src="http://localhost:3333/files/204781.jpg" />
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} />
-                            <img src={comment} />
-                            <img src={send} />
-                        </div>
-
-                        <strong>900 likes</strong>
-                        <p>
-                            Um post muito massa<span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
+                            <strong>{post.likes} likes</strong>
+                            <p>
+                                Um post muito massa
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))}
             </section>
         );
     }
