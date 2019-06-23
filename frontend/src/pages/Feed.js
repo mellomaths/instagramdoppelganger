fdd1d74acb8275908d64bed3bf5f9cc4ea483632
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-22 22:44:41 -0300

    Set state on socket post event

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
 import api from '../services/api';
 
@@ -15,9 +16,19 @@ export default class Feed extends Component {
     };
 
     async componentDidMount() {
+        this.registerToSocket();
+
         const response = await api.get('posts');
         this.setState({ feed: response.data.posts });
     }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        });
+    };
 
     handleLike = id => {
         api.post(`/posts/${id}/like`);
