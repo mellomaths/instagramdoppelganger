b2ce6f50f6521d9e53ff7febaae01d98d6a1044a
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-23 14:47:33 -0300

    Add header to routes

--- instapp/src/routes.js
+++ instapp/src/routes.js
@@ -1,10 +1,25 @@
+import React from 'react';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { Image } from 'react-native';
+
 import Feed from './pages/Feed';
 import New from './pages/Feed';
 
+import logo from './assets/logo.png';
+
 export default createAppContainer(
-    createStackNavigator({
-        Feed,
-        New
-    })
+    createStackNavigator(
+        {
+            Feed,
+            New
+        },
+        {
+            defaultNavigationOptions: {
+                headerTintColor: '#000',
+                headerTitle: <Image style={{ marginHorizontal: 30 }} source={logo} />,
+                headerBackTitle: null
+            },
+            mode: 'modal'
+        }
+    )
 );
